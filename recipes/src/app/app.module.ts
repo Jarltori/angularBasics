b57b333c90d8c0a30f31b1bfde8e5e11b04495c8
Jarltori <97944983+Jarltori@users.noreply.github.com>
2023-01-30 16:21:15 +0100

    Struktur (Components) & Header

--- recipes/src/app/app.module.ts
+++ recipes/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { recipeBookComponent } from './recipeBook/recipeBook-component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { RecipeListComponent } from './recipeBook/recipe-list/recipe-list.component';
+import { RecipeDetailComponent } from './recipeBook/recipe-detail/recipe-detail.component';
+import { RecipeItemComponent } from './recipeBook/recipe-list/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    recipeBookComponent,
+    ShoppingEditComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
---
+++ recipes/src/app/recipeBook/recipeBook-component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "app-recipeBook",
+    templateUrl: "./recipeBook-component.html"
+})
+
+
+export class recipeBookComponent{
+
+}
